872c1a52aaa33198cdbd85084be7a54ff01a4cc7
himoji <61588258+himoji@users.noreply.github.com>
2025-09-16 20:18:30 +0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,18 @@
 // @ts-check
-
+import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 import pluginHooks from 'eslint-plugin-react-hooks';
 import pluginRouter from '@tanstack/eslint-plugin-router';
+import pluginUnicorn from 'eslint-plugin-unicorn';
 import configPrettier from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config(
-  { ignores: ['eslint.config.mjs', 'vite.config.js'] },
+export default defineConfig([
+  { ignores: ['dist/*', 'eslint.config.mjs', 'vite.config.js'] },
   eslint.configs.recommended,
-  tseslint.configs.strictTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
   {
     languageOptions: {
       parserOptions: {
@@ -33,6 +34,21 @@ export default tseslint.config(
     },
   },
   pluginHooks.configs['recommended-latest'],
-  pluginRouter.configs['flat/recommended'],
-  configPrettier
-);
+  ...pluginRouter.configs['flat/recommended'],
+  {
+    rules: {
+      'max-lines-per-function': ['error', { max: 400 }],
+      'max-lines': ['error', { max: 500 }],
+    },
+  },
+  {
+    files: ['**/components/**/*'],
+    plugins: {
+      unicorn: pluginUnicorn,
+    },
+    rules: {
+      'unicorn/filename-case': ['error', { case: 'pascalCase' }],
+    },
+  },
+  configPrettier,
+]);
